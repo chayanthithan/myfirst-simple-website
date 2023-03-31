e42791287c3a76b45690a1988edf787cad408a37
chayanthithan <kumarchayan31@gmail.com>
2023-03-31 18:17:36 +0530

    first mycode

---
+++ ho.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>home</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+	<!-- font awesome link -->
+	<script src="https://kit.fontawesome.com/2568cb3989.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="banner">
+		<div class="navigation-bar">
+			<div class="container">
+				<div class="row">
+				<div class="logo-side col-2">
+					<i class="fa-solid fa-person-biking-mountain"><h4><span>uov</span>rent</h4></i>
+					
+				</div>
+				<div class="link-side col">
+					<ul>
+						<li><a href="#">home</a></li>
+						<li><a href="#">about us</a></li>
+						<li><a href="#">vehicle</a></li>
+						<li><a href="#">contact us</a></li>
+					</ul>
+				</div>
+				<div class="login-side col-3">
+					<button class="btn01">login</button>
+					<button class="btn02">register</button>
+				</div>
+			</div>
+			</div>
+			
+			
+		</div>
+		<div class="content-bar">
+			<div class="container ">
+				<div class="slogan">
+					<h2>drive your dream with <span>uovrent</span></h2>
+					<p>we are ready to provide best vehicles</p>
+				</div>
+				<div class="search">
+					<label>choose brands</label>
+					<select>
+						<option value="yamaha">yamaha</option>
+						<option value="bajaj">bajaj</option>
+						<option value="hero">hero</option>
+						<option value="honda">honda</option>
+						<option value="tvs">tvs</option>
+					</select>
+					<input type="text" name="search-box" placeholder="search here">
+					<button><i class="fa-solid fa-magnifying-glass"></i></button>
+				</div>
+				
+			</div>
+		</div>
+		<div class="footer-bar">
+			<div class="container">
+				<div class="row">
+					<div class="links col-4">
+						<h4>links</h4>
+						<ul>
+							<li><a href="">home</a></li>
+							<li><a href="">about us</a></li>
+							<li><a href="">contact us</a></li>
+							<li><a href="">vehicle </a></li>
+						</ul>
+					</div>
+					<div class="newsletter col-4">
+						<h4>newsletter</h4>
+						<form>
+							<input type="email" name="email" placeholder="enter email....">
+							<button type="submit">search</button>
+						</form>
+					</div>
+					<div class="contact-us col-4">
+						<h4>contact</h4>
+						<p>Copyright © 2023 uovrent. <br>All Rights Reserved.</p>
+						<div class="socialmedia">
+							<i class="fab fa-facebook"></i>
+							<i class="fab fa-twitter"></i>
+							<i class="fab fa-instagram"></i>
+							<i class="fab fa-linkedin"></i>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+
+	</div>
+</body>
+</html>
