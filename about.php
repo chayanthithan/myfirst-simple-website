6a98fce96675bbb533bdbc53881a37ebbf80e18d
chayanthithan <129511056+chayanthithan@users.noreply.github.com>
2023-03-31 18:34:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>about</title>
+	<link rel="stylesheet" type="text/css" href="about.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+	<!-- font awesome link -->
+	<script src="https://kit.fontawesome.com/2568cb3989.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="banner">
+		<div class="navigation-bar">
+			<div class="container">
+				<div class="row">
+					<div class="logo-side col-2">
+						<i class="fa-solid fa-person-biking-mountain"><h4><span>uov</span>rent</h4></i>
+					
+					</div>
+					<div class="link-side col">
+						<ul>
+							<li><a href="#">home</a></li>
+							<li><a href="#">about us</a></li>
+							<li><a href="#">vehicle</a></li>
+							<li><a href="#">contact us</a></li>
+						</ul>
+					</div>
+					<div class="login-side col-3">
+						<button type="submit" class="btn01">login</button>
+						<button type="submit" class="btn02">register</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content-bar">
+			<div class="container fieldset-div">
+				<form>
+				<fieldset>
+					<legend>about us</legend>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+				</fieldset>
+					
+				</form>
+				<div class="row">
+						<div class="vission-logo col-2">
+							<p>vission</p>
+						</div>
+						<div class="vission-text col-9">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+						</div>
+				</div>
+				<div class="row">
+						<div class="mission-logo col-2">
+							<p>mission</p>
+						</div>
+						<div class="mission-text col-9">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+						</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="footer-bar">
+			<div class="container">
+				<div class="row">
+					<div class="col links">
+						<h4>links</h4>
+						<ul>
+							<li><a href="">home</a></li>
+							<li><a href="">about us</a></li>
+							<li><a href="">contact us</a></li>
+							<li><a href="">vehicle </a></li>
+						</ul>
+					</div>
+					<div class="col newsletter">
+						<h4>newsletter</h4>
+						<form>
+							<input type="email" name="email" placeholder="enter email....">
+							<button type="submit">search</button>
+						</form>
+					</div>
+					<div class="col contact-us">
+						<h4>contact</h4>
+						<p>Copyright © 2023 uovrent. <br>All Rights Reserved.</p>
+						<div class="socialmedia">
+							<i class="fab fa-facebook"></i>
+							<i class="fab fa-twitter"></i>
+							<i class="fab fa-instagram"></i>
+							<i class="fab fa-linkedin"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
